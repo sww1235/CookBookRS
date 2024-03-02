6cd252f9cdfd9ce18f5c6653b2a02fcf73ca17df
Stephen Walker-Weinshenker <sww1235@sww1235.net>
2024-03-02 11:54:15 -0700

    added units to ingredient

--- src/datatypes/ingredient.rs
+++ src/datatypes/ingredient.rs
@@ -1,3 +1,5 @@
+use dimensioned::ucum;
+
 /// `Ingredient` is a unique item that represents the quantity of a particular ingredient
 #[derive(Debug, Clone, Default)]
 pub struct Ingredient {
@@ -7,6 +9,26 @@ pub struct Ingredient {
     pub name: String,
     /// optional description
     pub description: Option<String>,
-    //TODO: units
+    /// Unit of ingredient
+    pub unit: UnitType,
     //TODO: inventory reference
+}
+
+/// `UnitType` handles different unit types for an ingredient and allows flexibility rather than
+/// needing to have 1 ingredient type per unit type
+#[non_exhaustive]
+#[derive(Debug, Clone)]
+pub enum UnitType {
+    /// Represents a count or physical quantity of an `Ingredient`:
+    /// Ex: 30 chocolate chips, 5 bananas, 10 carrots etc.
+    Quantity(f64),
+    /// Mass of an `Ingredient`
+    Mass(ucum::Gram<f64>),
+    /// Volume of an `Ingredent`
+    Volume(ucum::Meter3<f64>),
+}
+impl Default for UnitType {
+    fn default() -> Self {
+        UnitType::Quantity(0.0_f64)
+    }
 }
