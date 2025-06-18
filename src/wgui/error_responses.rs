f9df2e0659ff6c1804db4be5b29227e851578c9b
Stephen Walker-Weinshenker <sww1235@sww1235.net>
2025-06-17 20:29:00 -0600

    fixed method_not_allowe() method to comply with http standards

--- src/wgui/error_responses.rs
+++ src/wgui/error_responses.rs
@@ -1,13 +1,22 @@
 use std::io::Empty;
 
-use tiny_http::{http::status::StatusCode, Response};
+use tiny_http::{
+    http::{header, method, status::StatusCode},
+    Response,
+};
 
 pub fn not_found() -> Response<Empty> {
     Response::empty(StatusCode::NOT_FOUND)
 }
 
-pub fn method_not_allowed() -> Response<Empty> {
-    Response::empty(StatusCode::METHOD_NOT_ALLOWED)
+pub fn method_not_allowed(allowed_methods: Vec<method::Method>) -> Response<Empty> {
+    let mut response = Response::empty(StatusCode::METHOD_NOT_ALLOWED);
+    response.add_header(
+        header::ALLOW,
+        header::HeaderValue::try_from(allowed_methods.iter().map(|i| i.to_string()).collect::<String>())
+            .expect("converting HTTP methods to strings failed"),
+    );
+    response
 }
 
 pub fn bad_request() -> Response<Empty> {
